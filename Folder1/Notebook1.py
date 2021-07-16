ad84ba3eeffe83204185f371272f16b5ec014554
alexott <alexott>
2021-07-16 14:47:40 +0000

    Add example of the repository with notebooks

---
+++ Folder1/Notebook1.py
@@ -0,0 +1,7 @@
+# Databricks notebook source
+# MAGIC %md
+# MAGIC 
+# MAGIC * <a href="$../MainNotebook">See main notebook</a>
+# MAGIC * <a href="$../Name%20with%20space">You can use also names with space (in parent folder)</a>
+# MAGIC * <a href="$./Subfolder2/Notebook2">Notebook in subfolder</a>
+# MAGIC * <a href="$./Subfolder2/Subfolder3/Notebook3">Notebook in subsubfolder</a>
---
+++ MainNotebook.py
@@ -0,0 +1,7 @@
+# Databricks notebook source
+# MAGIC %md
+# MAGIC 
+# MAGIC * <a href="$./Name%20with%20space">You can use also names with space (current folder)</a>
+# MAGIC * <a href="$./Folder1/Notebook1">Notebook in sub-subfolder</a>
+# MAGIC * <a href="$./Folder1/Subfolder2/Notebook2">Notebook in sub-subfolder</a>
+# MAGIC * <a href="$./Folder1/Subfolder2/Subfolder3/Notebook3">Notebook in sub-sub-subfolder</a>
